c17938147f44b0fe10c68b2787714a12c5095ae7
REJOINT <brecab120@gmail.com>
2023-05-22 22:30:27 -0400

    jwt-authentication updates

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('jwt')
 
 const { User } = db
 
@@ -14,8 +15,39 @@ router.post('/', async (req, res) => {
         res.status(404).json({
             message: `Could not find a user with the provided username and password`
         })
-    } else{
-        res.json({ user })
+    } else {
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: under.userId})
+        res.json({ user: user, token: result.value })
+    }
+})
+
+router.get('/profile', async (req, res) => {
+    try {
+        // Split the authorization header into [ "Bearer", "TOKEN" ]:
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+
+        // Only handle "Bearer" authorization for now
+        // (we could add other authorization strategies later): 
+        if (authenticationMethod == 'Bearer') {
+
+            // Decode the JWT
+
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+
+            // Get the logged in user's id from the payload
+            const { id } = result.value
+
+            // Find the user object using their id:
+        let user = await User.findOne({
+            where: {
+                userId:
+            }
+        })
+        res.json(user)
+      }
+
+    } catch {
+        res.json(null)
     }
 })
 
